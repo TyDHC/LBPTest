0b3d057d06f4844ef26b69587ee15a5f18be4713
TyDHC <sdudaihaichao@163.com>
2026-01-13 06:57:55 +0800

    1

--- LBPTest/MyNat.lean
+++ LBPTest/MyNat.lean
@@ -1,17 +1,47 @@
+-- Example/MyNat.lean
+
 import Architect
-import Mathlib.Tactic
 
+@[blueprint]
+inductive MyNat : Type where
+  | zero : MyNat
+  | succ : MyNat → MyNat
 
+namespace MyNat
+
+@[blueprint "def:nat-add"
+  (statement := /-- Natural number addition. -/)]
+def add (a b : MyNat) : MyNat :=
+  match b with
+  | zero => a
+  | succ b => succ (add a b)
 
 @[simp, blueprint
-  (statement := /-- For any natural number $a$, $0 + a = a$ -/)]
-theorem zero_add_test (a : ℕ) : a + 0 = a := by
+  (statement := /-- For any natural number $a$, $0 + a = a$,
+    where $+$ is \cref{def:nat-add}. -/)]
+theorem zero_add (a : MyNat) : add zero a = a := by
   /-- The proof follows by induction. -/
-  grind
+  induction a <;> simp [*, add]
 
 @[blueprint
   (statement := /-- For any natural numbers $a, b$,
     $(a + 1) + b = (a + b) + 1$. -/)]
-theorem add_comm_test (a b : ℕ) : a + b = b + a := by
+theorem succ_add (a b : MyNat) : add (succ a) b = succ (add a b) := by
   /-- Proof by induction on $b$. -/
-  grind
+  fun_induction a.succ.add b <;> grind [= add]
+@[blueprint
+  (statement := /-- For any natural numbers $a, b$,
+    $a + b = b + a$. -/)]
+theorem add_comm (a b : MyNat) : add a b = add b a := by
+  induction b with
+  | zero =>
+    have := trivial
+    /-- The base case follows from \cref{MyNat.zero_add}. -/
+    simp [add]
+  | succ b ih =>
+    /-- The inductive case follows from \cref{MyNat.succ_add}. -/
+    sorry_using [succ_add]  -- the `sorry_using` tactic declares dependency
+
+-- Additional content omitted
+
+end MyNat
